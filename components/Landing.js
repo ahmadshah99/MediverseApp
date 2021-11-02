45cb26c4fa2e6d6535af0149ebad3bc8623b7aa8
Ahmad Shah <ahmadali.shah@mail.utoronto.ca>
2021-11-02 06:20:59 -0400

    landing page

--- components/Landing.js
+++ components/Landing.js
@@ -1,11 +1,27 @@
-import React from 'React';
-import {StyleSheet, View} from 'react-native';
-import { Text } from 'react-native-elements';
+import React from 'react';
+import {StyleSheet, View, ScrollView, Image } from 'react-native';
+import { Text, Card, Icon } from 'react-native-elements';
+
+import landingPageMan from '../assets/images/landing-page-man.png'
+import landingPageMan2 from '../assets/images/landing-page-man-2.png'
+
+import CardButton from "./atoms/CardButton";
+
 
 const Landing = () => {
+    const doctorButtonIcon = <Icon name='doctor' type='fontisto' size={50} color='#fff'/>;
+    const medicineButtonIcon = <Icon name='heartbeat' type='font-awesome' size={50} color='#f50'/>;
+
     return (
-        <ScrollView>
+        <ScrollView contentContainerStyle={styles.mainView}>
+            <View style={styles.landingPageImages}>
+                <Image source={landingPageMan} style={{ height: 200, width: 173 }}/>
+                <Image source={landingPageMan2} style={{ height: 171, width: 190 }}/>
+            </View>
             <Text h1 style={styles.titleText}>All you need medical travel companion</Text>
+            <Text h4>What can we do for you?</Text>
+            <CardButton title="Find a doctor" icon={doctorButtonIcon} />
+            <CardButton title="Translate my medicine" icon={medicineButtonIcon} />
         </ScrollView>
     )
 };
@@ -13,8 +29,31 @@ const Landing = () => {
 export default Landing;
 
 const styles = StyleSheet.create({
+    mainView: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 10
+    },
+    landingPageImages: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
     titleText: {
         color: '#035762',
         fontWeight: 'bold'
     },
+    buttonView: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    cardButton: {
+        width: '85%',
+        height: 200,
+        backgroundColor: '#035762',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
---
+++ components/atoms/CardButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Card} from 'react-native-elements';
+
+const CardButton = (props) => {
+    return (
+        <Card containerStyle={styles.cardButton}>
+            <Card.Title style={{ color: '#fff' }}>{ props.title }</Card.Title>
+            { props.icon }
+        </Card>
+    )
+};
+
+export default CardButton;
+
+const styles = StyleSheet.create({
+    cardButton: {
+        width: '85%',
+        height: 200,
+        backgroundColor: '#035762',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
