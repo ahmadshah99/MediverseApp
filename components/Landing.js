64fc2057e127059013ea10ec0c33c18a7455a97e
Ahmad Shah <ahmadali.shah@mail.utoronto.ca>
2021-11-02 04:55:19 -0400

    more fonts

--- components/Landing.js
+++ components/Landing.js
@@ -1,14 +1,20 @@
 import React from 'React';
-import { Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import { Text } from 'react-native-elements';
 
 const Landing = () => {
     return (
         <View>
-            <Text>Hello World</Text>
+            <Text h1 style={styles.titleText}>All you need medical travel companion</Text>
         </View>
     )
 };
 
 export default Landing;
 
-
+const styles = StyleSheet.create({
+    titleText: {
+        color: '#035762',
+        fontFamily: 'OpenSans-Regular'
+    },
+});
