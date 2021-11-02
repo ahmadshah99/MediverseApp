2bb51d4b637027ad9cedba94a9dc1d2884013822
Ahmad Shah <ahmadali.shah@mail.utoronto.ca>
2021-11-02 06:25:52 -0400

    navigation

--- android/app/src/main/java/cscd90/team/mediverse/MainActivity.java
+++ android/app/src/main/java/cscd90/team/mediverse/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 
 import android.os.Bundle;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
@@ -12,7 +12,7 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.ReactActivityDelegateWrapper;
 
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
     // Added automatically by Expo Config
     @Override
@@ -32,14 +32,7 @@ public class MainActivity extends ReactActivity {
     super.onCreate(null);
   }
 
-  /**
-   * Returns the name of the main component registered from JavaScript.
-   * This is used to schedule rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "main";
-  }
+  
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
