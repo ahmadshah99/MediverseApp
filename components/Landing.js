35badd509c6020046ba2bf766d50962273d07255
Ahmad Shah <ahmadali.shah@mail.utoronto.ca>
2021-11-02 04:32:26 -0400

    basic files and dependencies

---
+++ components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'React';
+import { Text, View } from 'react-native';
+
+const Landing = () => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    )
+};
+
+export default Landing;
+
+
