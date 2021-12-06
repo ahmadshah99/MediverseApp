ed3944a1c0c6209afc304fe06a397a231b577d11
Raz Ben Haim <benhaim.raz@gmail.com>
2021-12-06 03:24:07 -0500

    Premium vs. freemium for filtering by distance and rating in browse doctors. Premium vs. freemium for language and expertise options for searching doctors.

--- Pages/BrowseDoctors.js
+++ Pages/BrowseDoctors.js
@@ -6,11 +6,22 @@ import DoctorCard from "../components/DoctorCard";
 import {getDoctorsBySearch} from "../api/Doctor";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
+import { Ionicons } from '@expo/vector-icons';
+import { storeData, getData } from '../utils/auth.js';
+import { AntDesign } from '@expo/vector-icons';
+
 
 const BrowseDoctors = ({ route, navigation }) => {
     // list of doctors to render
     const [doctors, setDoctors] = useState([])
     const [formattedLocation, setFormattedLocation] = useState(route.params.location)
+    const [filterBy, setFilterBy] = useState(0) //0 - nothing, 1 - rating, 2 - distance
+
+    
+    const API_URL = 'http://localhost:5001';
+    const [isPremium, setIsPremium] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [message, setMessage] = useState('');
 
     const renderCards = ({item }) => {
         return (
@@ -29,13 +40,44 @@ const BrowseDoctors = ({ route, navigation }) => {
         'sortBy': 'distance'
     }
 
-    useEffect(() => {
+    useEffect(async () => {
         console.log(searchData)
         getDoctorsBySearch(searchData).then(res => {
             console.log(res.data.doctors);
             setDoctors(res.data.doctors);
             setFormattedLocation(res.data.formattedSearchAddress)
-        } )
+        } ).catch(e => alert(e));
+
+        fetch(`${API_URL}/user/isPremium`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${await getData("jwt")}`
+            }
+        }).then(async res => {
+            try{
+                const jsonRes = await res.json();
+                console.log("Response: \n" + JSON.stringify(jsonRes));   
+                if (res.status === 200) {
+                    setIsError(false);
+                    setMessage("User profile data fetched successfully.");
+                    const isPrem = JSON.parse(JSON.stringify(jsonRes)).isPremium;
+                    setIsPremium(isPrem);
+                    if(isPrem){
+                        setLanguagesList(premiumLanguagesList);
+                        setExpertiseList(premiumExpertiseList);
+                    }else{
+                        setLanguagesList(basicLanguagesList);
+                        setExpertiseList(basicExpertiseList);
+                    }
+                } else {
+                    setIsError(true);
+                    setMessage(jsonRes.message);          
+                }
+            }catch(err){
+                console.log(err);
+            }
+        });
     }, [])
 
     const emptyDoctorFlatList = () => {
@@ -46,21 +88,50 @@ const BrowseDoctors = ({ route, navigation }) => {
         )
     }
 
+    function handleFilterByRating(){
+        setFilterBy(1);
+        setDoctors(doctors.sort((a, b) => b.rating.avg.$numberDecimal - a.rating.avg.$numberDecimal));
+    }
+
+    function handleFilterByDistance(){
+        setFilterBy(2);
+        getDoctorsBySearch(searchData).then(res => {
+            console.log(res.data.doctors);
+            setDoctors(res.data.doctors);
+            setFormattedLocation(res.data.formattedSearchAddress)
+        } ).catch(e => alert(e))
+    }
+
+
     return (
         <View style={styles.mainView}>
             <Header navigation={navigation}/>
-            <View style={{
-                display: 'flex',
-                justifySelf: 'flex-start'
-            }}>
-            </View>
+
+
             <FlatList
                 style = {{ marginTop: 50 }}
                 removeClippedSubviews
                 data={doctors}
                 renderItem={renderCards}
                 ListHeaderComponent={
-                    <Text>Showing results for <Text style={{ fontWeight: 'bold', fontStyle: 'italic'}}>{formattedLocation}</Text></Text>
+                    <View style={{paddingLeft: 30 }}>
+                        <Text>Showing results for <Text style={{ fontWeight: 'bold', fontStyle: 'italic'}}>{formattedLocation}</Text></Text>
+                        {isPremium ?
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Text style={{ marginTop: 10 }}>Sort Search Results By: </Text>
+                                    <Text style={{ fontWeight: "bold", marginTop: 10 }}>Rating </Text>
+                                    <Ionicons onPress={() => handleFilterByRating()} style = {{ marginTop: 10 }} name={filterBy === 1 ? "radio-button-on" : "radio-button-off"} size={24} color="black" />
+                                    <Text style={{ fontWeight: "bold", marginTop: 10 }}>  Distance </Text>
+                                    <Ionicons onPress={() => handleFilterByDistance()} style = {{ marginTop: 10 }} name={filterBy === 2 ? "radio-button-on" : "radio-button-off"} size={24} color="black" />
+                                </View>
+                                :
+                                <View style={{ flexDirection: "row" }}>
+                                    <AntDesign name="star" size={24} color="#53D8C7" />
+                                    <Text>To filter doctors by rating and distance, {'\n'} consider becoming a premium user.</Text>
+                                </View>
+                        }
+
+                    </View>
                 }
                 ListEmptyComponent={emptyDoctorFlatList()}
             />
--- Pages/SearchDoctors.js
+++ Pages/SearchDoctors.js
@@ -5,6 +5,9 @@ import Menu from '../components/Menu';
 import RNPickerSelect from 'react-native-picker-select';
 import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
 import Constants from 'expo-constants';
+import { storeData, getData } from '../utils/auth.js';
+import { AntDesign } from '@expo/vector-icons';
+
 
 const SearchDoctors = ({ navigation }) => {
     const [location, setLocation] = useState('')
@@ -12,7 +15,17 @@ const SearchDoctors = ({ navigation }) => {
     const [expertise, setExpertise] = useState('')
     const [language, setLanguage] = useState('')
     const [maxDistance, setMaxDistance] = useState(10)
-    const distanceList = [
+
+    const API_URL = 'http://localhost:5001';
+    const [isPremium, setIsPremium] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const [languagesList, setLanguagesList] = useState([]);
+    const [expertiseList, setExpertiseList] = useState([]);
+
+
+    const basicDistanceList = [
         {
             label: 'Any distance',
             value: 2000
@@ -42,7 +55,7 @@ const SearchDoctors = ({ navigation }) => {
             value: 50
         }]
 
-    const languagesList = [
+    const basicLanguagesList = [
         {
             label: 'Any language',
             value: ''
@@ -88,7 +101,141 @@ const SearchDoctors = ({ navigation }) => {
             value: 'Indonesian'
         }];
 
-    const expertiseList = [
+        const premiumLanguagesList = [
+            {
+                label: 'Any language',
+                value: ''
+            },
+            {
+            label: 'English',
+            value: 'English'
+            },
+            {
+                label: 'Mandarin',
+                value: 'Mandarin'
+            },
+            {
+                label: 'Hindi',
+                value: 'Hindi'
+            },
+            {
+                label: 'Spanish',
+                value: 'Spanish'
+            },
+            {
+                label: 'French',
+                value: 'French'
+            },
+            {
+                label: 'Arabic',
+                value: 'Arabic'
+            },
+            {
+                label: 'Bengali',
+                value: 'Bengali'
+            },
+            {
+                label: 'Russian',
+                value: 'Russian'
+            },
+            {
+                label: 'Portuguese',
+                value: 'Portuguese'
+            },
+            {
+                label: 'Indonesian',
+                value: 'Indonesian'
+            },
+            {
+                label: 'Hebrew',
+                value: 'Hebrew'
+            },
+            {
+                label: 'Japanese',
+                value: 'Japanese'
+            },
+            {
+                label: 'German',
+                value: 'German'
+            },
+            {
+                label: 'Chinese',
+                value: 'Chinese'
+            },
+            {
+                label: 'Polish',
+                value: 'Polish'
+            },
+            {
+                label: 'Korean',
+                value: 'Korean'
+            },
+            {
+                label: 'Vietnamese',
+                value: 'Vietnamese'
+            },
+            {
+                label: 'Turkish',
+                value: 'Turkish'
+            },
+            {
+                label: 'Hebrew',
+                value: 'Hebrew'
+            },
+            {
+                label: 'Romanian',
+                value: 'Romanian'
+            },
+            {
+                label: 'Dutch',
+                value: 'Dutch'
+            },
+            {
+                label: 'Greek',
+                value: 'Greek'
+            },
+            {
+                label: 'Swedish',
+                value: 'Swedish'
+            },
+            {
+                label: 'Italian',
+                value: 'Italian'
+            },
+            {
+                label: 'Thai',
+                value: 'Thai'
+            },
+        ];
+
+        const basicExpertiseList = [
+            {
+                label: 'Any expertise',
+                value: ''
+            },
+            {
+                label: 'Family Medicine',
+                value: 'Family Medicine'
+            },
+            {
+                label: 'Geriatric Medicine',
+                value: 'Geriatric Medicine'
+            },
+            {
+                label: 'Gynecology',
+                value: 'Gynecology'
+            },
+            {
+                label: 'Ophthalmology',
+                value: 'Ophthalmology'
+            },
+            {
+                label: 'Pediatric',
+                value: 'Pediatric'
+            },
+          ];
+
+    const premiumExpertiseList = [
         {
             label: 'Any expertise',
             value: ''
@@ -138,6 +285,40 @@ const SearchDoctors = ({ navigation }) => {
             value: 'Urology'
         }];
 
+
+        useEffect(async () => {
+            fetch(`${API_URL}/user/isPremium`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `bearer ${await getData("jwt")}`
+                }
+            }).then(async res => {
+                try{
+                    const jsonRes = await res.json();
+                    console.log("Response: \n" + JSON.stringify(jsonRes));   
+                    if (res.status === 200) {
+                        setIsError(false);
+                        setMessage("User profile data fetched successfully.");
+                        const isPrem = JSON.parse(JSON.stringify(jsonRes)).isPremium;
+                        setIsPremium(isPrem);
+                        if(isPrem){
+                            setLanguagesList(premiumLanguagesList);
+                            setExpertiseList(premiumExpertiseList);
+                        }else{
+                            setLanguagesList(basicLanguagesList);
+                            setExpertiseList(basicExpertiseList);
+                        }
+                    } else {
+                        setIsError(true);
+                        setMessage(jsonRes.message);          
+                    }
+                }catch(err){
+                    console.log(err);
+                }
+            });
+        }, [])
+
     return (
         <View style={styles.mainView}>
             <Text h1 style={styles.titleText}>Find a Doctor</Text>
@@ -156,6 +337,10 @@ const SearchDoctors = ({ navigation }) => {
                         lightTheme
                         round
                     />
+                    <View style={{ flexDirection: "row", marginBottom: 10 }}>
+                        <AntDesign name="star" size={24} color="#53D8C7" />
+                        <Text>To see more options for doctors and languages, consider becoming a premium user.</Text>
+                    </View>
                 </View>
                 <View style={styles.picker}>
                     <Text h5 style={{ color: '#000000', fontWeight: 'bold' }}>Select language</Text>
@@ -175,7 +360,7 @@ const SearchDoctors = ({ navigation }) => {
                     <Text h5 style={{ color: '#000000', fontWeight: 'bold' }}>Max search distance:</Text>
                     <RNPickerSelect
                         onValueChange={value => setMaxDistance(value)}
-                        items={distanceList}
+                        items={basicDistanceList}
                     />
                 </View>
             </View>
